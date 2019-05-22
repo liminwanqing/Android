3c4eaf16eceaeb035e430da20a30c8e93132e5a1
李珉 <limin@meizu.com>
2019-05-22 10:46:02 +0800

    Add mvp pinmin model.

---
+++ mvppinmin/app/src/main/java/com/example/mvppinmin/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mvppinmin;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends BaseActivity implements MvpView{
+
+    TextView text;
+
+    MvpPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text);
+
+        //初始化Presenter
+        presenter = new MvpPresenter();
+        presenter.attachView(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //断开View引用
+        presenter.detachView();
+    }
+
+    @Override
+    public void showData(String data) {
+        text.setText(data);
+    }
+
+    // button 点击事件调用方法
+    public void getData(View view){
+        presenter.getData("normal");
+    }
+
+    // button 点击事件调用方法
+    public void getDataForFailure(View view){
+        presenter.getData("failure");
+    }
+
+    // button 点击事件调用方法
+    public void getDataForError(View view){
+        presenter.getData("error");
+    }
+
+}
---
+++ mvppinmin/app/src/main/java/com/example/mvppinmin/MvpView.java
@@ -0,0 +1,10 @@
+package com.example.mvppinmin;
+
+public interface MvpView extends BaseView{
+    /**
+     * 当数据请求成功后，调用此接口显示数据
+     * @param data 数据源
+     */
+    void showData(String data);
+
+}
