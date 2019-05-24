f5c323ffe37ab7379decb3bb438a4b30529db2a7
李珉 <limin@meizu.com>
2019-05-24 16:02:03 +0800

    update mvp shishang

---
+++ mvpshishang/app/src/main/java/com/example/mvpshishang/DataModel.java
@@ -0,0 +1,21 @@
+package com.example.mvpshishang;
+
+public class DataModel {
+    public static BaseModel request(String token){
+
+        // 声明一个空的BaseModel
+        BaseModel model = null;
+
+        try {
+            //利用反射机制获得对应Model对象的引用
+            model = (BaseModel)Class.forName(token).newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return model;
+    }
+}
---
+++ mvpshishang/app/src/main/java/com/example/mvpshishang/Token.java
@@ -0,0 +1,9 @@
+package com.example.mvpshishang;
+
+public class Token {
+    // 包名
+    private static final String PACKAGE_NAME = "com.example.mvpshishang";
+
+    // 具体Model
+    public static final String API_USER_DATA = PACKAGE_NAME + ".UserDataModel";
+}
