3d2bba21844da2896e4317ee6ee92ea4b52829cf
李珉 <limin@meizu.com>
2019-06-04 16:48:55 +0800

    rename mvvmqigai

---
+++ mvvmqigai/app/src/main/java/com/example/myapplication/weather/model/WeatherData.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.weather.model;
+
+public class WeatherData {
+
+    private WeatherInfo weatherinfo;
+
+    public WeatherInfo getWeatherinfo() {
+        return weatherinfo;
+    }
+
+    public void setWeatherinfo(WeatherInfo weatherinfo) {
+        this.weatherinfo = weatherinfo;
+    }
+}
